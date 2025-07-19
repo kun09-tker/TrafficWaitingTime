285dd2c88ebed522b7c4c45fb22b39e2968492a9
kun09-tker <ltanloc97o@gmail.com>
2025-07-19 15:06:47 +0700

    .

--- API/traffic.py
+++ API/traffic.py
@@ -11,7 +11,7 @@ load_dotenv()
 API_KEY = os.environ["TRAFFIC_API_KEY"]
 
 def call_traffic_api(desc, route_points):
-
+    datas = []
     for lat, lon, distance in route_points:
         url = ("https://api.tomtom.com/traffic/services/4/flowSegmentData/absolute/10/json"
                f"?point={lat},{lon}&unit=KMPH&key={API_KEY}")
@@ -31,16 +31,24 @@ def call_traffic_api(desc, route_points):
         data["wind_kph"] = wind_kph
         data["condition_text"] = condition_text
 
-        conn = database.connect_to_database()
-        database.create_database(conn)
-        database.save_value(conn, data)
-        conn.close()
+        datas.append(data)
+
+    return datas
 
 if __name__ == "__main__":
     with open('locations.json', 'r', encoding='utf-8') as f:
         LOCATIONS = json.load(f)["locations"]
+    datas = []
+
+    conn = database.connect_to_database()
+    database.create_database(conn)
 
     for location in LOCATIONS:
         desc = location["desciption"]
         routes = location["routes"]
-        call_traffic_api(desc, routes)
+        datas.extend(call_traffic_api(desc, routes))
+
+    for data in datas:
+        database.save_value(conn, data)
+
+    conn.close()
